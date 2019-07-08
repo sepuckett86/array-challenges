5d9e7731e0e29cc3add64bb5a30584eb7ef1ee54
sepuckett86 <sepuckett86@gmail.com>
2019-07-08 16:01:17 -0700

    add two more tests for 15 and 113

--- test/multiples-of-n.test.js
+++ test/multiples-of-n.test.js
@@ -1,9 +1,17 @@
 const multiplesOfN = require('../lib/multiples-of-n');
 
 describe('multiplesOfN', () => {
-  it('returns an array of positive integers that are multiples of n', () => {
+  it('returns integers in range 1-100 that are multiples of 5', () => {
     const multiplesOf5 = multiplesOfN(5);
     expect(multiplesOf5).toEqual([5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]);
+  });
+  it('returns integers in range 1-100 that are multiples of 15', () => {
+    const multiplesOf5 = multiplesOfN(15);
+    expect(multiplesOf5).toEqual([15, 30, 45, 60, 75, 90]);
+  });
+  it('returns integers in range 1-100 that are multiples of 113', () => {
+    const multiplesOf5 = multiplesOfN(113);
+    expect(multiplesOf5).toEqual([]);
   });
 });
 
