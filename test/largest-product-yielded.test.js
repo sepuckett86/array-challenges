48b2c4cf6be79246c22e7da2f2e2ed69f404265d
sepuckett86 <sepuckett86@gmail.com>
2019-07-08 16:41:34 -0700

    added more tests and conditionals in function largest-product-yielded

--- test/largest-product-yielded.test.js
+++ test/largest-product-yielded.test.js
@@ -1,9 +1,19 @@
 const largestProductYielded = require('../lib/largest-product-yielded');
 
 describe('largestProductYielded', () => {
-  it('test here', () => {
+  it('returns largest product', () => {
     const arr = [-10, 7, 29, 30, 5, -10, -70];
     const product = largestProductYielded(arr);
     expect(product).toBe(21000);
+  });
+  it('returns largest product even if no negatives', () => {
+    const arr = [7, 29, 30];
+    const product = largestProductYielded(arr);
+    expect(product).toBe(6090);
+  });
+  it('returns largest product even if one positive', () => {
+    const arr = [7, -29, -30];
+    const product = largestProductYielded(arr);
+    expect(product).toBe(6090);
   });
 });
