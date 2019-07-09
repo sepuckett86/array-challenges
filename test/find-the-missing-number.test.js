3f57afff6d3487deb944e2fb387817e22210db6a
sepuckett86 <sepuckett86@gmail.com>
2019-07-08 17:16:23 -0700

    add back upper and lower bounds to pass extra test

--- test/find-the-missing-number.test.js
+++ test/find-the-missing-number.test.js
@@ -1,12 +1,26 @@
 const findTheMissingNumber = require('../lib/find-the-missing-number');
 
 describe('findTheMissingNumber', () => {
-  it('tests', () => {
+  it('finds the missing number', () => {
     const arr = [2, 5, 1, 4, 9, 6, 3, 7];
     const upper = 9;
     const lower = 1;
     const missingNumber = findTheMissingNumber(arr, upper, lower);
     expect(missingNumber).toBe(8);
+  });
+  it('finds the missing number if it is the lower bound', () => {
+    const arr = [2, 5, 8, 4, 9, 6, 3, 7];
+    const upper = 9;
+    const lower = 1;
+    const missingNumber = findTheMissingNumber(arr, upper, lower);
+    expect(missingNumber).toBe(1);
+  });
+  it('finds the missing number if it is the upper bound', () => {
+    const arr = [2, 5, 8, 4, 1, 6, 3, 7];
+    const upper = 9;
+    const lower = 1;
+    const missingNumber = findTheMissingNumber(arr, upper, lower);
+    expect(missingNumber).toBe(9);
   });
 });
 
